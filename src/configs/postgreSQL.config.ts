c578fee0ef3df645b961c469fd565be8f251d6d0
Nguyen Van Dao <nvdao003@gmail.com>
2026-03-19 15:58:48 +0700

    chore: setup drizzle orm connect postgreSQL

---
+++ src/configs/postgreSQL.config.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
+import * as schema from '../db/schema'
+import './env.config'
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+})
+
+export const db = drizzle(pool, { schema })
+
+export const connectDB = async () => {
+  try {
+    await pool.connect()
+    console.log('Connected to PostgreSQL')
+  } catch (error) {
+    console.error('Failed to connect to PostgreSQL:', error)
+    process.exit(1)
+  }
+}
